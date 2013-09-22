2d79a6a8e727bfd0b5d6f5b44a47961b7553ea86
Richard Serna <Bigpapa1>
2013-09-22 15:45:48 -0700

    Add custom dynamic titles

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,56 +1,77 @@
 require 'spec_helper'
 
 describe "Static  pages" do 
-	let(:title) {"Ruby on Rails Tutorial Sample App "}
+	let(:title) {"Ruby on Rails Tutorial Sample App"}
+	
 	context "Home Page" do 
-		it "Should have the content 'Sample App'" do 
+		it "Should have the h1 'Sample App'" do 
 			visit '/static_pages/home'
-			page.should have_content('Sample App')
+			page.should have_selector('h1', text: 'Sample App')
 		end
 
-		it "Should have the right title" do 
+		it "should have the base title" do
+      visit '/static_pages/home'
+      page.should have_selector('title',
+                        :text => "Ruby on Rails Tutorial Sample App")
+    end
+
+		it "should not have a custome page title" do 
 			visit '/static_pages/home'
-			page.should have_selector('title',
-										text: "#{title}| Home")
+			page.should_not have_selector('title', text:'| Home')
 		end
 	end
 
 	context "Help Page" do 
-		it "Should have the content 'Help'" do 
+		it "Should have the h1 'Help'" do 
 			visit '/static_pages/help'
-			page.should have_content('Help')
+			page.should have_selector('h1', text: 'Help')
 		end
 
-		it "should have Help in the title" do 
+		it "Should have the base title" do 
 			visit '/static_pages/help'
 			page.should have_selector('title',
-										text: "#{title}| Help")
+										text: "#{title}")
+		end
+
+		it "should not have a custome page title" do 
+			visit '/static_pages/help'
+			page.should_not have_selector('title', text:'| Help')
 		end
 	end
 
 	context "About page" do 
-		it "Should have the content 'About Us'" do 
+		it "Should have the h1 'About Us'" do 
 			visit '/static_pages/about'
-			page.should have_content('About Us')
+			page.should have_selector('h1', text: 'About Us')
 		end
 
-		it "Should have About in the title" do 
-			visit "/static_pages/about"
+		it "Should have the base title" do 
+			visit '/static_pages/about'
 			page.should have_selector('title',
-											text: "#{title}| About")
-		end 
+										text: "#{title}")
+		end
+
+		it "should not have a custome page title" do 
+			visit '/static_pages/about'
+			page.should_not have_selector('title', text:'| About Us')
+		end
 	end
 
 	context "Contact" do 
-		it "should have the content 'Contact'" do 
+		it "Should have the h1 'Contact'" do 
 			visit '/static_pages/contact'
-			page.should have_content('Contact')
-		end 
+			page.should have_selector('h1', text: 'Contact')
+		end
 
-		it "should have Contact in the title" do 
-			visit "/static_pages/contact"
-			page.should have_selector("title",
-											text: "#{title}| Contact")
+		it "Should have the base title" do 
+			visit '/static_pages/contact'
+			page.should have_selector('title',
+										text: "#{title}")
+		end
+
+		it "should not have a custome page title" do 
+			visit '/static_pages/contact'
+			page.should_not have_selector('title', text:'| Contact')
 		end
 	end
 
